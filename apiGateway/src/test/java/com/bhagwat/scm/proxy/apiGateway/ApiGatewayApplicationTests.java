ee9009c381fdfdc3e7470c9008d1779fec12e904
prasannakrishna.b <prasannakrishna2011@gmail.com>
2024-12-10 17:27:02 +0530

    added servers

---
+++ apiGateway/src/test/java/com/bhagwat/scm/proxy/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhagwat.scm.proxy.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ registryServer/src/test/java/com/bhagwat/scm/eureka/registryServer/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhagwat.scm.eureka.registryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
